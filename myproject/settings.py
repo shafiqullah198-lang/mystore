3b2f527818d9d0eaae4f92718fda18bc32902844
shafiqullah198-lang <shafiqullah198@gmail.com>
2026-05-12 18:18:16 +0500

    new changes

--- myproject/settings.py
+++ myproject/settings.py
@@ -174,15 +174,23 @@ SIMPLE_JWT = {
 }
 
 # CORS Settings
-CORS_ALLOW_ALL_ORIGINS = True  # Set to False and specify CORS_ALLOWED_ORIGINS in production
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:54369",
-    "http://127.0.0.1:54369",
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
 ]
 CORS_ALLOW_HEADERS = [
     "accept",
+    "accept-encoding",
     "authorization",
     "content-type",
+    "dnt",
+    "origin",
     "user-agent",
     "x-csrftoken",
     "x-requested-with",
